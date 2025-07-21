efb13ac90891ae02814914fc9e7cf3b065c4d3ca
matusala3 <matusalakiflom@gmail.com>
2025-07-21 12:49:45 +0300

    Update header files: modify include guards, add function prototypes, and update timestamps

--- include/pipex.h
+++ include/pipex.h
@@ -6,20 +6,20 @@
 /*   By: magebreh <magebreh@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 22:38:46 by magebreh          #+#    #+#             */
-/*   Updated: 2025/07/12 22:49:18 by magebreh         ###   ########.fr       */
+/*   Updated: 2025/07/15 22:49:24 by magebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-#define PIPEX_H
+# ifndef PIPEX_H
+#  define PIPEX_H
 
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include "../libft/libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include "../libft/libft.h"
 
 typedef struct s_pipex
 {
@@ -27,10 +27,18 @@ typedef struct s_pipex
 	char	*outfile;
 	char 	*cmd1;
 	char	*cmd2;
-	char	*envp;
+	char	**envp;
 	int		pipe_fd[2];
 }	t_pipex;
 
-
+char	*join_paths(char **folders, char *cmd);
+char	*get_cmd_path(char *cmd, char **envp);
+char	*get_path_var(char **envp);
+void	free_string_array(char **split);
+void	child2_redirect(t_pipex *pipex);
+void	child1_process(t_pipex *pipex);
+void	child1_redirect(t_pipex *pipex);
+void	child2_process(t_pipex *pipex);
+void	execute_pipeline(t_pipex *pipex);
 
 #endif
--- include/pipex_bonus.h
+++ include/pipex_bonus.h
@@ -6,47 +6,43 @@
 /*   By: magebreh <magebreh@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:47:16 by magebreh          #+#    #+#             */
-/*   Updated: 2025/07/12 22:15:37 by magebreh         ###   ########.fr       */
+/*   Updated: 2025/07/15 22:52:41 by magebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PIPEX_H
-#define FT_PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include "../libft/libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include "../libft/libft.h"
 
+typedef struct s_pipex
+{
+	int		here_doc;
+	int		num_cmds;
+	char	*infile;
+	char	*outfile;
+	char	**cmds;
+	char	**envp;
+	char	*limiter;
+}	t_pipex;
 
-typedef struct s_pipex {
-    int     here_doc;
-    int     num_cmds;
-    char    *infile;
-    char    *outfile;
-    char    **cmds;
-    char    **envp;
-    char    *limiter;
-}   t_pipex;
-
-int parse_arguments(t_pipex *pipex, int argc, char **argv, char **envp);
-int init_here_doc_pipe(t_pipex *pipex);
-int setup_pipe(int *pipe_fd, int cmd_index, int total_cmds);
-int exec_cmd(t_pipex *pipex, int cmd_index, int fd_in, int *pipe_fd);
-
-void setup_child_fds(t_pipex *pipex, int i, int prev_fd, int pipe_fd[2]);
-void execute_command(t_pipex *pipex, int i);
-void launch_child(t_pipex *pipex, int i, int prev_fd, int pipe_fd[2]);
-int exec_loop(t_pipex *pipex, int fd_in);
-int execute_pipeline(t_pipex *pipex);
-
-char *get_path_variable(char **envp);
-char *get_command_path(char *cmd, char **envp);
-char *join_path(char **folders, char *cmd);
-void free_string_array(char **split);
-
+int		parse_arguments(t_pipex *pipex, int argc, char **argv, char **envp);
+int		init_here_doc_pipe(t_pipex *pipex);
+int		setup_pipe(int *pipe_fd, int cmd_index, int total_cmds);
+void	setup_child_fds(t_pipex *pipex, int i, int prev_fd, int pipe_fd[2]);
+void	execute_command(t_pipex *pipex, int i);
+void	launch_child(t_pipex *pipex, int i, int prev_fd, int pipe_fd[2]);
+int		exec_loop(t_pipex *pipex, int fd_in);
+int		execute_pipeline(t_pipex *pipex);
+char	*get_path_variable(char **envp);
+char	*get_command_path(char *cmd, char **envp);
+char	*join_path(char **folders, char *cmd);
+void	free_string_array(char **split);
 
 #endif
